ef96810d3d392a143597d0b380c21e91d054a643
pooja yadav <misspooju@gmail.com>
2024-06-10 08:40:32 +0530

    get loggedin user api created

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 
-const { registerUser, loginUser, getUsers } = require('../userController/index');
-const { userRegisterValidate,userLoginValidate } = require('../utils/userValidation');
-const {ensureAuthenticated} = require('../utils/auth');
+const { registerUser, loginUser, getUsers, getUser } = require('../userController/index');
+const { userRegisterValidate, userLoginValidate } = require('../utils/userValidation');
+const { ensureAuthenticated } = require('../utils/auth');
 
-routes.post('/register', userRegisterValidate,registerUser)
+routes.post('/register', userRegisterValidate, registerUser);
 
-routes.post('/login',userLoginValidate,loginUser)
+routes.post('/login', userLoginValidate, loginUser);
 
-routes.get('/users', ensureAuthenticated,getUsers);
+routes.get('/users', ensureAuthenticated, getUsers);
+
+routes.get('/user', ensureAuthenticated, getUser);
 
 module.exports = routes;
